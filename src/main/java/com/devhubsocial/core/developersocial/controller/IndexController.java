872641d8eab8aaf8cbc900102df7d6386370cd45
stevenmcdonald <engineers2526@gmail.com>
2020-05-05 17:41:42 -0500

    added IndexController

--- src/main/java/com/devhubsocial/core/developersocial/controller/IndexController.java
+++ src/main/java/com/devhubsocial/core/developersocial/controller/IndexController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/")
 public class IndexController {
 
-    @GetMapping
+    @GetMapping("/")
     @ResponseBody
     public String welcome() {
         return "Welcome to Devhub Social";
