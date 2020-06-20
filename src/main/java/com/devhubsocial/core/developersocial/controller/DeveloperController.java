6df7ba104b2cd75f8bf2805b989e94dbc80b1e70
stevenmcdonald <engineers2526@gmail.com>
2020-06-20 11:35:58 -0500

    fix lombok annotations

---
+++ src/main/java/com/devhubsocial/core/developersocial/controller/DeveloperController.java
@@ -0,0 +1,9 @@
+package com.devhubsocial.core.developersocial.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/developers")
+public class DeveloperController {
+}
