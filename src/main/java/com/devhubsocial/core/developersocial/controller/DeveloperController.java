df1713a51ccefa2abc5403c7ba8cce898c994cd4
stevenmcdonald <engineers2526@gmail.com>
2020-06-26 20:53:39 -0500

    test

--- src/main/java/com/devhubsocial/core/developersocial/controller/DeveloperController.java
+++ src/main/java/com/devhubsocial/core/developersocial/controller/DeveloperController.java
@@ -1,10 +1,28 @@
 package com.devhubsocial.core.developersocial.controller;
 
+import com.devhubsocial.core.developersocial.entities.Developer;
+import com.devhubsocial.core.developersocial.repository.DeveloperRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api/v1/developers")
 public class DeveloperController {
+
+
+    private DeveloperRepository developerRepository;
+
+    @Autowired
+    public DeveloperController(DeveloperRepository developerRepository) {
+        this.developerRepository = developerRepository;
+    }
+
+    @GetMapping
+    public Flux<Developer> getAllDevelopers() {
+       return this.developerRepository.findAll();
+    }
 
 }
