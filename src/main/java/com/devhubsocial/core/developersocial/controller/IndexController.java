27fa560eaac539fed7ad62d3c0c2488544aec98b
stevenmcdonald <engineers2526@gmail.com>
2020-07-08 18:08:27 -0500

    test

--- src/main/java/com/devhubsocial/core/developersocial/controller/IndexController.java
+++ src/main/java/com/devhubsocial/core/developersocial/controller/IndexController.java
@@ -2,15 +2,17 @@ package com.devhubsocial.core.developersocial.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Flux;
 
 @Controller
 public class IndexController {
 
+
     @GetMapping("/test")
     @ResponseBody
-    public String welcome() {
-        return "Welcome to Devhub Social";
+    public Flux<String> getRoot() {
+        return Flux.just("Welecome to devhub social.");
     }
+
 }
